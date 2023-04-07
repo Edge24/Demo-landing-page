21b37398877130775bcbef07424ccab1315d99d2
Aritra Bera <111326696+Edge24@users.noreply.github.com>
2023-04-07 17:05:04 +0530

    done

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// Side nav start //
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+// Side nav end //
+
+// Search pannel start //
+
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Search pannel end //
